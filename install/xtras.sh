1b955a0b102ff9d66bf990c8ab47b6d7f585e1c5
Nickolas Lanasa <nicklanasa@gmail.com>
2025-07-08 07:38:20 -0400

    Moar updates

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,7 +1,10 @@
+sudo sed -i '/^\s*#\[multilib\]/,/^$/{s/^\s*#//}' /etc/pacman.conf
+sudo pacman -Sy
+
 yay -S --noconfirm --needed \
   signal-desktop spotify dropbox-cli zoom \
   obsidian-bin typora libreoffice obs-studio kdenlive \
-  pinta xournalpp firefox
+  pinta xournalpp firefox xpadneo-dkms-git steam
 
 # Copy over Omarchy applications
 source ~/.local/share/omarchy/bin/omarchy-sync-applications || true
